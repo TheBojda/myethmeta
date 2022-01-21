8ae11d2fbb8bff5ff2298ee674088015d48576e3
Laszlo Fazekas <thebojda@gmail.com>
2022-01-21 10:41:00 +0100

    remove web3 dependency + fetch meta

--- libraries/js/myethmeta/src/index.ts
+++ libraries/js/myethmeta/src/index.ts
@@ -1,24 +1,62 @@
-import Web3 from 'web3';
-import { Contract } from 'web3-eth-contract';
-import { AbiItem } from 'web3-utils';
-
 import { EthereumAddressMetadataJSONSchema } from "./myethmeta-schema"
-import myethmeta_abi from './myethmeta_abi.json'
 
 const CONTRACT_ADDRESS = "0x63Ba8dfAEBa09a63c1bCB47a46229f14707Af995";
+const POLYGON_RPC_URL = "https://polygon-rpc.com/";
 
 export default class MyEthMetaClient {
 
-  private contract: Contract;
+  private requestId = 1;
 
-  constructor() {
-    const web3 = new Web3((window as any).ethereum);
-    this.contract = new web3.eth.Contract(myethmeta_abi as AbiItem[], CONTRACT_ADDRESS);
+  constructor() { }
+
+  private hexToBytes(hex: string): number[] {
+    let bytes = [];
+    for (let c = 0; c < hex.length; c += 2)
+      bytes.push(parseInt(hex.substring(c, c + 2), 16));
+    return bytes;
+  }
+
+  private async callContract(address: string) {
+    const prefix = '0xd7f84684000000000000000000000000'; // firs 4 bytes of keccak('getMetaURI(address)')
+    const data = prefix + address.substring(2)
+    const payload = {
+      id: this.requestId,
+      jsonrpc: "2.0",
+      method: "eth_call",
+      params: [
+        {
+          data: data,
+          to: CONTRACT_ADDRESS
+        }, "latest"
+      ]
+    }
+    const response = await fetch(POLYGON_RPC_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
+    this.requestId++;
+    const json_result = await response.json();
+    const result = json_result.result;
+    const result_bytes = this.hexToBytes(result.substring(2)); // cut 0x
+    let result_string_bytes = result_bytes.slice(32 + 31, result_bytes.length); // cut offset and zeros
+    const string_length = result_string_bytes.shift(); // get string legth
+    const string_bytes = result_string_bytes.slice(0, string_length); // get string data
+    return String.fromCharCode(...string_bytes); // convert to string
+  }
+
+  private getGatewayURL(uri: string) {
+    if (uri.startsWith("ipfs://"))
+      return "https://ipfs.io/ipfs/" + uri.substring(7);
+    return uri;
   }
 
   async getMetaData(address: string): Promise<EthereumAddressMetadataJSONSchema> {
-    const uri = await this.contract.methods.getMetaURI(address).call();
-    return uri;
+    const uri = await this.callContract(address);
+    const response = await fetch(this.getGatewayURL(uri));
+    return await response.json();
   }
 
 }
